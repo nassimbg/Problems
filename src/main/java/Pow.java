1317036512b4503fc751e5440cf5f4a4edd7f24c
BOU GHANNAM Nassim <nassimboughannam@gmail.com>
2017-10-19 10:01:23 +0300

    Power

--- src/main/java/Pow.java
+++ src/main/java/Pow.java
@@ -2,26 +2,21 @@ public class Pow {
 
    public static double myPow(double x, int n) {
 
-      if (n < 0) {
-         x = 1/x;
-         n = -1 * n;
+      double temp = x;
+      if (n == 0) {
+         return 1;
       }
-      double va = x;
-
-      int i;
-      for ( i = 2; i <= n; i = 2 * i) {
-         va= va * va;
-      }
-
-
-
-      if (n > 1) {
-         for (int j = 0; j < n - i / 2; j++) {
-            va *= x;
+      temp = myPow(x, n / 2);
+      if (n % 2 == 0) {
+         return temp * temp;
+      } else {
+         if (n > 0) {
+            return x * temp * temp;
+         } else {
+            return (temp * temp) / x;
          }
-
       }
-      return n == 0 ? 1 : va;
+
 
    }
 }
