49fb025f375cb683ec0c995bc911d84f070497d3
BOU GHANNAM Nassim <nassimboughannam@gmail.com>
2017-11-14 23:02:46 +0200

    Gas Station

---
+++ src/main/java/GasStation.java
@@ -0,0 +1,14 @@
+public class GasStation {
+   public static int canCompleteCircuit(int[] gas, int[] cost) {
+      int n = gas.length;
+      int total = 0, subsum = Integer.MAX_VALUE, start = 0;
+      for(int i = 0; i < n; ++i){
+         total += gas[i] - cost[i];
+         if(total < subsum) {
+            subsum = total;
+            start = i + 1;
+         }
+      }
+      return (total < 0) ?  -1 : (start%n);
+   }
+}
