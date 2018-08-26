14da2bf3153cdfb24c82b4782876ba1f3a41c720
nboughannam <nassim.boughannam@murex.com>
2018-08-26 21:55:33 +0300

    add other solutions

--- src/main/java/CombinationSumII.java
+++ src/main/java/CombinationSumII.java
@@ -1,11 +1,59 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("Duplicates")
 public class CombinationSumII {
 
+
+
    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+      Arrays.sort(candidates);
+      List<List<Integer>> result = new ArrayList<>();
+      dfs(candidates, target, new HashMap<>(), 0, result);
+
+      return result;
+   }
+
+   private static void dfs(int[] candidates, int sum, Map<Integer, Integer> count, int index, List<List<Integer>> result) {
+      if (sum < 0) {
+         return;
+      }
+      if (sum == 0) {
+         List<Integer> solution = new ArrayList<>();
+         for(Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            int value = entry.getKey();
+
+            for(int counter = 0; counter < entry.getValue(); counter++) {
+               solution.add(value);
+            }
+         }
+
+         result.add(solution);
+      } else if (index < candidates.length) {
+         int currentValue = candidates[index];
+         int aleardyTaken = count.getOrDefault(currentValue, 0);
+         int lastIndexDiffFromCandidate = index - aleardyTaken - 1;
+
+         dfs(candidates, sum, count, index + 1, result);
+
+         if (lastIndexDiffFromCandidate < 0 || candidates[lastIndexDiffFromCandidate] != currentValue) {
+            count.put(currentValue, aleardyTaken + 1);
+
+            dfs(candidates, sum - currentValue, count, index + 1, result);
+
+            if (aleardyTaken == 0) {
+               count.remove(currentValue);
+            } else {
+               count.put(currentValue, aleardyTaken - 1);
+            }
+         }
+      }
+   }
+
+   public static List<List<Integer>> combinationSum2DP(int[] candidates, int target) {
       Arrays.sort(candidates);
 
       // initialize the dp array
--- src/main/java/FactorCombinations.java
+++ src/main/java/FactorCombinations.java
@@ -6,7 +6,39 @@ import java.util.Map;
 public class FactorCombinations {
 
    private Map<Integer, List<List<Integer>>> dp = new HashMap<>();
+
+
+
+
    public List<List<Integer>> getFactors(int n) {
+      List<List<Integer>> result = new ArrayList<>();
+
+      dfs(result, n, 2, new ArrayList<>());
+      return result;
+   }
+
+   private static void dfs(List<List<Integer>> result, int target, int start,  List<Integer> temp) {
+      if (target > 1) {
+         for(int multiplier = start; multiplier < target; ++multiplier) {
+
+            int multiplicent = target / multiplier;
+
+            if (target % multiplier == 0 && multiplicent >= multiplier) {
+               temp.add(multiplier);
+               temp.add(multiplicent);
+               result.add(new ArrayList<>(temp));
+               temp.remove(temp.size() - 1);
+
+               // todo pass the multiplier as a start point to decompose the multiplicent so that the decomposed factors are >= to multiplier
+               // todo => preserve the asc order in the resulting array
+               dfs(result, multiplicent , multiplier, temp);
+               temp.remove(temp.size() - 1);
+            }
+         }
+      }
+   }
+
+   public List<List<Integer>> getFactors2(int n) {
 
       List<List<Integer>> result = dp.getOrDefault(n, new ArrayList<>());
 
