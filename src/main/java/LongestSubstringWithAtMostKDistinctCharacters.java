e5a127dfde95a0afced58958b646d72e69f75834
nboughannam <nassim.boughannam@murex.com>
2018-08-30 23:32:48 +0300

    Longest Substring with At Most K Distinct Characters

--- src/main/java/LongestSubstringWithAtMostKDistinctCharacters.java
+++ src/main/java/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -1,5 +1,59 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubstringWithAtMostKDistinctCharacters {
+
+
    public static String findSubString(String s, int k) {
+      Map<Character, Integer> freq = new HashMap<>();
+
+      int left = 0;
+      int right = 0;
+
+      int maxLength = 0;
+      int currentLength= 0;
+
+      int maxLeft = 0;
+
+      while(left <= right && right < s.length() && left < s.length()) {
+
+         while(freq.size() <= k && right < s.length()) {
+            char currentRightChar = s.charAt(right++);
+            int frequency = freq.getOrDefault(currentRightChar , 0);
+
+            freq.put(currentRightChar , frequency + 1);
+            currentLength++;
+            if (freq.size()<=k && currentLength > maxLength) {
+               maxLength = currentLength;
+               maxLeft = left;
+            }
+
+         }
+
+         while(freq.size() > k && left < s.length()) {
+            char currentLeftChar = s.charAt(left++);
+            int frequency = freq.get(currentLeftChar) - 1;
+
+            if (frequency > 0) {
+               freq.put(currentLeftChar, frequency - 1);
+            } else {
+               freq.remove(currentLeftChar);
+            }
+            currentLength--;
+         }
+
+         if (freq.size()<=k && currentLength > maxLength) {
+            maxLength = currentLength;
+            maxLeft = left;
+         }
+      }
+
+      return s.substring(maxLeft, maxLength + maxLeft);
+   }
+
+
+
+   public static String findSubString2(String s, int k) {
 
       if(s == null || s.isEmpty()) {
          return s;
