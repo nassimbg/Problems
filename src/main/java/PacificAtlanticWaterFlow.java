8df39a6e87a1ca3062c7bb528f4925b9d127bc9b
nboughannam <nassim.boughannam@murex.com>
2018-08-31 00:51:19 +0300

    Pacific Atlantic Water Flow

--- src/main/java/PacificAtlanticWaterFlow.java
+++ src/main/java/PacificAtlanticWaterFlow.java
@@ -3,8 +3,48 @@ import java.util.List;
 
 public class PacificAtlanticWaterFlow {
 
+
    private static int[][] directions = new int[][] { { 1, 0 },  { -1, 0 },  { 0, 1 },  { 0, -1 }};
+
+
    public static List<int[]> pacificAtlantic(int[][] matrix) {
+      List<int[]> result = new ArrayList<>();
+
+      if(matrix.length == 0 || matrix[0].length == 0) {
+         return result;
+      }
+
+      int[][] visited = new int[matrix.length][matrix[0].length];
+
+      dfs(matrix, Integer.MAX_VALUE, 0, 0, visited, true, result);
+      dfs(matrix, Integer.MAX_VALUE, matrix.length - 1, matrix[0].length - 1, visited, false, result);
+
+      return result;
+   }
+
+   private static void dfs(int[][] matrix, int parentHeight, int row, int column, int[][] visited, boolean isPacific, List<int[]> result) {
+      if (row >= 0 && row < matrix.length && column >= 0 && column < matrix[0].length
+        && (isPacific ? 1 & visited[row][column] : 2 & visited[row][column]) == 0) {
+
+
+         if ((isPacific ? (row == 0 || column == 0) : (row == matrix.length - 1 || column == matrix[0].length - 1))
+           || parentHeight <= matrix[row][column]) {
+
+            visited[row][column] += (isPacific ? 1 : 2);
+
+            if (visited[row][column] == 3) {
+               result.add(new int[] {row, column});
+            }
+            for(int[] dir : directions) {
+               dfs(matrix, matrix[row][column], row + dir[0], column + dir[1], visited, isPacific, result);
+            }
+         }
+      }
+   }
+
+
+
+   public static List<int[]> pacificAtlantic2(int[][] matrix) {
       List<int[]> result = new ArrayList<>();
       int rows = matrix.length;
 
