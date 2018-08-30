1cd9005eb84c66d53e6e52cd66690309d42cd5c9
nboughannam <nassim.boughannam@murex.com>
2018-08-30 18:32:06 +0300

    Divide Two Integers

---
+++ src/test/java/DivideTwoIntegersTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DivideTwoIntegersTest {
+
+   @Test
+   public void divide() {
+      assertEquals(3, DivideTwoIntegers.divide(10, 3));
+   }
+
+
+   @Test
+   public void divide4() {
+      assertEquals(-3, DivideTwoIntegers.divide(-10, 3));
+   }
+
+   @Test
+   public void divide5() {
+      assertEquals(-2, DivideTwoIntegers.divide(7, -3));
+   }
+
+   @Test
+   public void divide2() {
+      assertEquals(3, DivideTwoIntegers.divide(15, 5));
+   }
+
+   @Test
+   public void divide3() {
+      assertEquals(3, DivideTwoIntegers.divide(Integer.MIN_VALUE, 5));
+   }
+}
