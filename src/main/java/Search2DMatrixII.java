228b3eabcc09665d79b66751354bc42c835bca51
nboughannam <nassim.boughannam@murex.com>
2018-09-02 16:58:21 +0300

    Search 2D Matrix II

--- src/main/java/Search2DMatrixII.java
+++ src/main/java/Search2DMatrixII.java
@@ -1,7 +1,56 @@
 import java.util.Arrays;
 
 public class Search2DMatrixII {
+
+
+
+
    public static boolean searchMatrix(int[][] matrix, int target) {
+      return searchBasedOnRow(matrix, target, 0 , matrix.length - 1);
+   }
+
+   private static boolean searchBasedOnRow(int[][] matrix, int target, int start, int end) {
+      if (start <= end) {
+         int row = (end+start) >>> 1;
+         int[] currentRow = matrix[row];
+
+         if (currentRow[0] > target) {
+            return searchBasedOnRow(matrix, target, start, row - 1);
+         } else if (currentRow[currentRow.length - 1] < target) {
+            return searchBasedOnRow(matrix, target, row + 1, end);
+         } else {
+
+            if (searchColumns(target, currentRow)) {
+               return true;
+            }
+
+            return searchBasedOnRow(matrix, target, start, row - 1) || searchBasedOnRow(matrix, target, row + 1, end);
+         }
+      }
+      return false;
+   }
+
+   private static boolean searchColumns(int target, int[] currentRow) {
+      int columnStart = 0;
+      int columnEnd = currentRow.length - 1;
+
+      while (columnStart <= columnEnd) {
+         int mid = (columnStart + columnEnd) >>> 1;
+
+         if (currentRow[mid] == target) {
+            return true;
+         }
+
+         if (currentRow[mid] > target) {
+            columnEnd = mid - 1;
+         } else {
+            columnStart = mid + 1;
+         }
+      }
+      return false;
+   }
+
+   public static boolean searchMatrix2(int[][] matrix, int target) {
       if (matrix.length == 0 || matrix[0].length == 0) {
          return false;
       }
