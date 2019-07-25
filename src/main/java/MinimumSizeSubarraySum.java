a52271404708d963f4c406b6948cca231469c3f8
NassimBG <nassimboughannam@gmail.com>
2019-07-25 16:59:49 +0300

    MinimumSizeSubarraySum

--- src/main/java/MinimumSizeSubarraySum.java
+++ src/main/java/MinimumSizeSubarraySum.java
@@ -1,5 +1,44 @@
 public class MinimumSizeSubarraySum {
+
+
    public static int minSubArrayLen(int s, int[] nums) {
+      int bestLeft = 0;
+      int bestRight = Integer.MAX_VALUE;
+
+      int left = 0;
+      int right = -1;
+      int sum = 0;
+      while(right < nums.length - 1) {
+
+         while (right < nums.length - 1 && sum < s) {
+            sum += nums[++right];
+         }
+
+         if (sum >= s) {
+            if (bestRight - bestLeft > right - left) {
+               bestRight = right;
+               bestLeft = left;
+            }
+         }
+
+         while (left <= right && sum >= s) {
+            if (bestRight - bestLeft > right - left) {
+               bestRight = right;
+               bestLeft = left;
+            }
+
+            sum -= nums[left++];
+         }
+      }
+
+      if (bestRight == Integer.MAX_VALUE) {
+         bestRight = -1;
+      }
+
+      return bestRight - bestLeft + 1;
+   }
+
+   public static int minSubArrayLen2(int s, int[] nums) {
       int minLength = Integer.MAX_VALUE;
       int sum = 0;
 
