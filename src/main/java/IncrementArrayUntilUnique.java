09d58886fa53234b346e5b540e7a182a776c0869
NassimBG <nassimboughannam@gmail.com>
2019-08-02 01:01:13 +0300

    IncrementArrayUntilUnique.java

--- src/main/java/IncrementArrayUntilUnique.java
+++ src/main/java/IncrementArrayUntilUnique.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class IncrementArrayUntilUnique {
    public static int getMinimumUniqueSum(List<Integer> arr) {
@@ -27,5 +30,23 @@ public class IncrementArrayUntilUnique {
       }
 
       return sum;
+   }
+
+
+
+   // another way using a set to check if we used this value before
+   // in fact we dont need this set since we should be sure if A[i-1] > A[i] => we used A[i] before
+   public int minIncrementForUnique(int[] A) {
+      Arrays.sort(A);
+      Set<Integer> used = new HashSet<>();
+      int res = 0;
+      for(int i=0; i<A.length; i++){
+         if(used.contains(A[i])){
+            res+=A[i-1]+1-A[i];
+            A[i]=A[i-1]+1;
+         }
+         used.add(A[i]);
+      }
+      return res;
    }
 }
